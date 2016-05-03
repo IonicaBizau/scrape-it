dc7f099dd7e82687476d19a804500e8eeaca15de
Ionică Bizău <bizauionica@gmail.com>
2016-05-03 08:32:15 +0300

    Promise example

--- example/index.js
+++ example/index.js
@@ -2,6 +2,19 @@
 
 const scrapeIt = require("../lib");
 
+// Promise interface
+scrapeIt("http://ionicabizau.net", {
+    title: ".header h1"
+  , desc: ".header h2"
+  , avatar: {
+        selector: ".header img"
+      , attr: "src"
+    }
+}).then(page => {
+    console.log(page);
+});
+
+// Callback interface
 scrapeIt("http://ionicabizau.net", {
     // Fetch the articles
     articles: {
