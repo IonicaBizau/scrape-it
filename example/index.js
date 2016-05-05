549149695d57a946caecce15e4db9aca59b90c1a
Ionică Bizău <bizauionica@gmail.com>
2016-05-05 06:58:24 -0700

    Fix #20. Fixed the example.

--- example/index.js
+++ example/index.js
@@ -2,44 +2,89 @@
 
 const scrapeIt = require("../lib");
 
-function parseStatusCurrent(description){
-    var status = {};
-    status.description = description.trim();
-    if(status.description == 'All Systems Operational'){
-        status.indicator = "Operational";
-        status.color = "green";
+// Promise interface
+scrapeIt("http://ionicabizau.net", {
+    title: ".header h1"
+  , desc: ".header h2"
+  , avatar: {
+        selector: ".header img"
+      , attr: "src"
     }
-    else if(status.description.indexOf('Minor')>-1){
-        status.indicator = "Not Fully Operational";
-        status.color = "yellow";
-    }
-    else {
-        status.indicator = "Not Fully Operational";
-        status.color = "red";
-    }
-    return status;
-}
+}).then(page => {
+    console.log(page);
+});
 
-scrapeIt("https://status.airbrake.io/", {
-    status: {
-	selector: ".page-status span.status",
-        data: {}
-	convert:  parseStatusCurrent
-    }
-  , provider: {
-        selector: ".powered-by"
-        , convert: (function (text) {
-            if(text == "Powered by StatusPage.io"){
-                return "StatusPage.io";
+// Callback interface
+scrapeIt("http://ionicabizau.net", {
+    // Fetch the articles
+    articles: {
+        listItem: ".article"
+      , data: {
+
+            // Get the article date and convert it into a Date object
+            createdAt: {
+                selector: ".date"
+              , convert: x => new Date(x)
             }
-            else {
-                return "Unknown";
+
+            // Get the title
+          , title: "a.article-title"
+
+            // Nested list
+          , tags: {
+                listItem: ".tags > span"
             }
-        })
+
+            // Get the content
+          , content: {
+                selector: ".article-content"
+              , how: "html"
+            }
+        }
     }
-  , updated_at: {
-        convert: new Date().toISOString()
+
+    // Fetch the blog pages
+  , pages: {
+        listItem: "li.page"
+      , name: "pages"
+      , data: {
+            title: "a"
+          , url: {
+                selector: "a"
+              , attr: "href"
+            }
+        }
     }
-}).then(status => {
-    console.log(status);
-}).catch(console.log);
+
+    // Fetch some other data from the page
+  , title: ".header h1"
+  , desc: ".header h2"
+  , avatar: {
+        selector: ".header img"
+      , attr: "src"
+    }
+}, (err, page) => {
+    console.log(err || page);
+});
+// { articles:
+//    [ { createdAt: Mon Mar 14 2016 00:00:00 GMT+0200 (EET),
+//        title: 'Pi Day, Raspberry Pi and Command Line',
+//        tags: [Object],
+//        content: '<p>Everyone knows (or should know)...a" alt=""></p>\n' },
+//      { createdAt: Thu Feb 18 2016 00:00:00 GMT+0200 (EET),
+//        title: 'How I ported Memory Blocks to modern web',
+//        tags: [Object],
+//        content: '<p>Playing computer games is a lot of fun. ...' },
+//      { createdAt: Mon Nov 02 2015 00:00:00 GMT+0200 (EET),
+//        title: 'How to convert JSON to Markdown using json2md',
+//        tags: [Object],
+//        content: '<p>I love and ...' } ],
+//   pages:
+//    [ { title: 'Blog', url: '/' },
+//      { title: 'About', url: '/about' },
+//      { title: 'FAQ', url: '/faq' },
+//      { title: 'Training', url: '/training' },
+//      { title: 'Contact', url: '/contact' } ],
+//   title: 'Ionică Bizău',
+//   desc: 'Web Developer,  Linux geek and  Musician',
+//   avatar: '/images/logo.png' }
